c7bf263c72bf9d18514bc7fb00273054f38dca4e
taowenyin <wenyin.tao@163.com>
2019-08-04 14:43:34 +0800

    添加天地图地图和标记服务

--- src/app/pages/cesium/cesium.component.ts
+++ src/app/pages/cesium/cesium.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from 'rxjs';
+import {bindCallback, Observable} from 'rxjs';
 import {AcNotification, ActionType} from 'angular-cesium';
 import {PlanesService} from '../../services/planes.service';
 import {map} from 'rxjs/operators';
@@ -15,19 +15,89 @@ export class CesiumComponent implements OnInit {
 
     private cesiumView = null;
 
+    // 天地图的密钥
+    private tdtAccessToken = '5685d82109cd055413fd7e3815b827be';
+
     constructor(private planesService: PlanesService) {}
 
     ngOnInit() {
+        // 配置Ion的密钥
         Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkNzA2YTE4Yi0zN2E3LTQwN2MtODliOS1hNjZiMGE3YjhjMDYiLCJpZCI6MTM1NDQsInNjb3BlcyI6WyJhc3IiLCJnYyJdLCJpYXQiOjE1NjM0NTIzNTF9.A5EnyeVHLL9GU6vI2qsw2KHD-q1rkbohTLyJwJysoWg';
 
+        // 初始化信息球
         this.cesiumView = new Cesium.Viewer('cesiumContainer', {
-            terrainProvider: Cesium.createWorldTerrain(),
-            scene3DOnly: true,
-            selectionIndicator: true,
-            baseLayerPicker: true
+            animation: false, // 是否创建动画小器件，左下角仪表
+            timeline: false, // 是否显示时间轴
+            sceneModePicker: false, // 是否显示3D/2D选择器
+            baseLayerPicker: true, // 是否显示图层选择器
+            geocoder: false, // 是否显示geocoder小器件，右上角查询按钮
+            scene3DOnly: true, // 如果设置为true，则所有几何图形以3D模式绘制以节约GPU资源
+            navigationHelpButton: false, // 是否显示右上角的帮助按钮
+            homeButton: false, // 是否显示Home按钮
+            infoBox: true, // 是否显示信息框
+            showRenderLoopErrors: false, // 如果设为true，将在一个HTML面板中显示错误信息
+            // imageryProviderViewModels: [imgTdtSl, imgTdtYx], // 图层列表
+            // selectedImageryProviderViewModel: imgTdtYx // 默认图层
         });
 
-        this.cesiumView.scene.glob.enableLighting = false;
+        // 去除版权信息
+        this.cesiumView._cesiumWidget._creditContainer.style.display = 'none';
+
+        // 创建天地图矢量
+        const imgTdtSl = new Cesium.ProviderViewModel({
+            name: '天地图矢量',
+            tooltip: '天地图矢量',
+            iconUrl: './assets/images/map.png',
+            creationFunction: function() {
+                this.cesiumView.imageryLayers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+                    url: 'http://t0.tianditu.gov.cn/cva_w/wmts?tk=5685d82109cd055413fd7e3815b827be',
+                    layer: 'cva',
+                    tileMatrixSetID: 'w',
+                    format: 'tiles',
+                    style: 'default'
+                }));
+
+                return new Cesium.WebMapTileServiceImageryProvider({
+                    url: 'http://t0.tianditu.gov.cn/vec_w/wmts?tk=5685d82109cd055413fd7e3815b827be',
+                    layer: 'vec',
+                    tileMatrixSetID: 'w',
+                    format: 'tiles',
+                    style: 'default'
+                });
+            }.bind(this)
+        });
+
+        // 创建天地图影像
+        const imgTdtYx = new Cesium.ProviderViewModel({
+            name: '天地图影像',
+            tooltip: '天地图影像',
+            iconUrl: './assets/images/map.png',
+            creationFunction: function() {
+                this.cesiumView.imageryLayers.addImageryProvider(new Cesium.WebMapTileServiceImageryProvider({
+                    url: 'http://t0.tianditu.gov.cn/cia_w/wmts?tk=5685d82109cd055413fd7e3815b827be',
+                    layer: 'cia',
+                    tileMatrixSetID: 'w',
+                    format: 'tiles',
+                    style: 'default'
+                }));
+
+                return new Cesium.WebMapTileServiceImageryProvider({
+                    url: 'http://t0.tianditu.gov.cn/img_w/wmts?tk=5685d82109cd055413fd7e3815b827be',
+                    layer: 'img',
+                    tileMatrixSetID: 'w',
+                    format: 'tiles',
+                    style: 'default'
+                });
+            }.bind(this)
+        });
+
+        // 创建图层列表
+        const providerViewModels = [imgTdtSl, imgTdtYx];
+        // 设置图层列表
+        this.cesiumView.baseLayerPicker.viewModel.imageryProviderViewModels = providerViewModels;
+        // 设置默认图层
+        this.cesiumView.baseLayerPicker.viewModel.selectedImagery = providerViewModels[1];
+
 
         // const titleset = this.cesiumView.scene.primitives.add(new Cesium.Cesium3DTileset({
         //     url: Cesium.IonResource.fromAssetId(34834)
